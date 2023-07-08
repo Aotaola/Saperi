f0a63450af46a401be60670b19a4acce79428f2f
Aotaola <115048502+Aotaola@users.noreply.github.com>
2023-07-07 21:38:12 -0400

    found asssociation error

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -1,6 +1,35 @@
 class SourcesController < ApplicationController
+    before_action :set_source, only: [:show, :edit, :update]
+
+    def show
+
+    end
+    def edit
+
+    end
+    def update
+        if @source.update
+            notice 'Source updated successfully'
+        else
+            flash[:error] = "error while updating source"
+            render :edit
+        end
+    end
+    def create
+        @source = Source.create(source_params)
+            if @source.save
+                notice 'Source was successfully created'
+            else
+                flash[:error] = 'an error occurred while creating'
+                flash[:notice] = source.errors.full_messages.to_sentence
+            end
+    end
 
     private
+
+    def set_source
+        @source = Source.find(params[:id])
+    end
 
     def source_params
         params.require(:source).permit(:id, :description, :link)
--- app/models/source.rb
+++ app/models/source.rb
@@ -1,9 +1,8 @@
 class Source < ApplicationRecord
-    belongs_to :user
-    belongs_to :collection
+    belongs_to :user, foreign_key: 'users_id'
+    
 
     validates :user, presence: true
-    validates :collection, presence: true
-    # validates :description, presence: true, lenght: {minimum: 1, maximum: 25}
-    # validates :link, presence: true, format: { with: /\A#{URI::regexp(['http', 'https'])}\z/, message: 'must be a valid URL' }
+    #validates :description, presence: true, lenght: {minimum: 1, maximum: 25}
+    #validates :link, presence: true, format: { with: /\A#{URI::regexp(['http', 'https'])}\z/, message: 'must be a valid URL' }
 end
