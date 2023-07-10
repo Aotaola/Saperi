af6683f022ed355eb35ab782b1f19e8634b82789
Aotaola <115048502+Aotaola@users.noreply.github.com>
2023-07-10 12:52:04 -0400

    fixups

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,6 +1,7 @@
 class Collection < ApplicationRecord
     has_one :forum
-    has_many :users
+    has_many :user_collections
+    has_many :users, through: :user_collections
 
     validates :title, presence: true, length: {minimum: 5, maximum: 80}
     validates :description, length: {minimum: 5, maximum: 250}
--- app/serializers/forum_serializer.rb
+++ app/serializers/forum_serializer.rb
@@ -1,3 +1,6 @@
 class ForumSerializer < ActiveModel::Serializer
   attributes :id
+  has_many :user_forums
+  has_many :users, through: :user_forums
+  
 end
