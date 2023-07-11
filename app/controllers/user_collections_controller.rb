b03bbba13724cb454e8a5f98dce311c80360141f
Aotaola <115048502+Aotaola@users.noreply.github.com>
2023-07-11 12:33:40 -0400

    creating user_cols

--- app/controllers/user_collections_controller.rb
+++ app/controllers/user_collections_controller.rb
@@ -1,2 +1,29 @@
 class UserCollectionsController < ApplicationController
+    before_action :set_collection, only: [:show, :destroy, :create]
+    before_action :set_user_col
+
+    def show
+
+    end
+    def index
+
+    end
+    def create
+        if @user_col.save
+            redirect @collection, notice: 'You have added this collection to your profile'
+        else
+            redirect @collection, notice: 'something went wrong'
+        end
+    end
+    def destroy
+
+    end
+
+    private 
+    def set_user_collection
+        @user_col = UserCollection.new(user: current_user, collection: @collection)
+    end
+    def set_collection
+       @collection = Collection.find(params[:collection_id])
+    end
 end
--- app/models/user_collection.rb
+++ app/models/user_collection.rb
@@ -1,4 +1,7 @@
 class UserCollection < ApplicationRecord
   belongs_to :user
   belongs_to :collection
+
+  validates :user_id, presence: true
+  validates :collection_id, presence: true
 end
