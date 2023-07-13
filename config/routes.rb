c0067e14ffdd3ef140d9212d81c85a787232b2e8
Aotaola <115048502+Aotaola@users.noreply.github.com>
2023-07-12 21:55:54 -0400

    stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Rails.application.routes.draw do
-  resources :sources, only: [:show, :index, :edit, :update, :create, :destroy]
+  resources :sources, only: [:show, :index, :new, :edit, :update, :create, :destroy]
   resources :user_collections, only: [:show, :index, :create, :destroy]
   resources :user_forums, only: [:show, :create, :destroy]
-  resources :users, only: [:new, :show, :edit, :update, :destroy]
+  resources :users, only: [:new, :create, :show, :edit, :update, :destroy]
   resources :forums, only: [:create, :destroy, :show]
   resources :collections, only: [:new, :create, :show, :edit, :update, :destroy]
-  resources :sessions, only: [:new, :create, :destroy]
+ 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root 'application#home'
-  get "/about", to: 'application#about'
+  get '/about', to: 'application#about'
+  delete '/logout', to: 'sessions#destroy', as: :logout
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   
-  
-  # get "/index", to: 'collections#index'
+
   
 end
