6420a5160faba5d1d84634373b7a6f44f83ae70a
Aotaola <115048502+Aotaola@users.noreply.github.com>
2023-09-06 11:16:39 -0400

    got rid of character constraint in collection description

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -6,7 +6,7 @@ class Collection < ApplicationRecord
     has_one :forum
 
     validates :title, presence: true, length: {minimum: 5, maximum: 80}
-    validates :description, length: {minimum: 5, maximum: 250}
+    validates :description, length: {minimum: 5}
     
     attr_accessor :user_id
 
